73da0f5101563c4fe07e2317513c3f484bffd0f2
Jaime Mejia-Beher <jmejiabeher@gmail.com>
2024-04-20 09:55:42 -0600

    first commit, html template first draft

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("hello world")
+
+
+const submitBtn = document.getElementById("submit")
+submitBtn.addEventListener("click", () => {
+    // grab the unique id && city input
+    const city = document.getElementById("city")
+    const input = document.getElementById("userInput")
+   
+    try {
+    const grabData = ()=>{
+        console.log("data")
+    }
+    grabData()
+    }
+    catch{
+
+    }
+    })
