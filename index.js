9064b319f6417c405c52c0b2c23e6d00851a464d
Jaime Mejia-Beher <jmejiabeher@gmail.com>
2024-04-30 21:07:50 -0600

    created filterUsername, userHireDate

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ submitBtn.addEventListener("click", () => {
     const input = document.getElementById("userName")
     const department = document.getElementById("department")
     const fullOrPartTime = document.getElementById("fullOrPartTime")
-    const filterEmp = Employee.filter(item => {      // store filter output
+    const filterEmpArray = Employee.filter(item => {      // store filter output
     if (item.address.city === station.value && item.department === department.value && item.roles.indexOf(fullOrPartTime.value) != -1) {      // if the city matches the input city from user
         return true;
     } else {
@@ -64,13 +64,30 @@ submitBtn.addEventListener("click", () => {
     }
    })
 
-   const filterUsername = Employee.filter( item => {
+   const filterUsername = Employee.filter( item => { // user employee
         if (item.username === input.value) {
             return true;
         } else {
             return false;
         }
    })
-   console.log(filterEmp)
-    console.log(filterUsername)
+    console.log(filterEmpArray)
+    console.log(filterUsername) // crazypanda
+    
+    const userHireDate = filterUsername[0].hiredOn  // crazypanda hire date
+    let userHireTime = new Date(userHireDate).getTime()  // converting a date string into a number to compare...
+    let userRank = 1 // user is set to highest seniority
+
+    for (let i=0; i < filterEmpArray.length; i++) {  // looping through filter emp array 
+        let currentEmpDate = filterEmpArray[i].hiredOn;  
+        let empHiredDate = new Date(currentEmpDate).getTime()
+        console.log(empHiredDate)  
+        console.log(userHireTime)
+        if (empHiredDate < userHireTime) {  // compared the two values
+            userRank++;         // increase the rank
+        } 
+    }
+    console.log(userRank)
     })
+
+    
